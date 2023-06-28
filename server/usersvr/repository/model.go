fccaaf53cc4cdb64919984382fea6f4aabeeb1d6
Percygu <380059082@qq.com>
2023-06-28 16:58:03 +0800

    update .gitignore

--- server/usersvr/repository/model.go
+++ server/usersvr/repository/model.go
@@ -2,7 +2,7 @@ package repository
 
 type User struct {
 	// gorm.Model
-	Id              int64  `gorm:"column:id; primary_key;"`
+	Id              int64  `gorm:"column:id; primary_key; auto_increment"`
 	Name            string `gorm:"column:user_name"`
 	Password        string `gorm:"column:password"`
 	Follow          int64  `gorm:"column:follow_count"`
